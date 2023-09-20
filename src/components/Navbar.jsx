cf39b9506965f53d82385947ef954adcb834baf7
Bob Dempsey <bobdempsey83@gmail.com>
2023-09-20 10:47:19 -0400

    updates navbar component to tailwindcss

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,15 @@
 import { Link } from "react-router-dom";
 import { ShoppingCart } from "phosphor-react";
-import "./navbar.css";
+
 const Navbar = () => {
   return (
-    <div className="navbar">
-      <div className="links">
-        <Link to="/">Shop</Link>
-        <Link to="/cart">
+    <div className="bg-slate-800 text-white flex items-center justify-between h-16 p-4">
+      <div className="text-2xl font-medium tracking-tight">
+        <Link to="/">React eCommerce</Link>
+      </div>
+      <div className="flex items-center space-x-6 font-medium text-lg">
+        {/* <Link to="/">Shop</Link> */}
+        <Link to="/cart" className="p-4">
           <ShoppingCart size={32} />
         </Link>
       </div>
