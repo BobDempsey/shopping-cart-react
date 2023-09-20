cc91a8e4425b99035477c59bde2cd153f3972921
Bob Dempsey <bobdempsey83@gmail.com>
2023-09-20 09:53:44 -0400

    updates cart and shop context

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -1,5 +1,29 @@
-const cart = () => {
-  return <div>cart</div>;
+import { PRODUCTS } from "../../products";
+import { ShopContext } from "../../context/shop-context";
+import { useContext } from "react";
+import CartItem from "./cart-item";
+import "./cart.css";
+
+const Cart = () => {
+  const { cartItems } = useContext(ShopContext);
+  return (
+    <div className="cart">
+      <div>
+        <h1>Your Cart Items</h1>
+      </div>
+      <div className="cartItems">
+        {PRODUCTS.map((product) => {
+          if (cartItems[product.id] === 0) return null;
+          return <CartItem data={product} key={product.id} />;
+        })}
+      </div>
+      <div className="checkout">
+        <p>Subtotal: $</p>
+        <button>Continue Shopping</button>
+        <button>Checkout</button>
+      </div>
+    </div>
+  );
 };
 
-export default cart;
+export default Cart;
